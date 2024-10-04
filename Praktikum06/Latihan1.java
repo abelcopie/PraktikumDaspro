19865152d1c3ba3cebde08ec71ff985533e42599
amandatodaze <amandatodaze@gmail.com>
2024-10-04 10:33:47 +0700

    --

--- Praktikum06/Latihan1.java
+++ Praktikum06/Latihan1.java
@@ -7,12 +7,16 @@ public class Latihan1 {
         if (Bil1 > Bil2) {
             if (Bil1 > Bil3){
                 System.out.println("Bilangan Terbesar " + Bil1);
-            } else
+            } else{
                 System.out.println("Bilangan Terbesar " + Bil2);
-        } else 
+            }
+                
+        } else {
             if (Bil2 > Bil1){
                 System.out.println("Bilangan Terbesar " + Bil2);
-            } else
+            } else {
                 System.out.println("Bilangan Terbesar " + Bil1);
-        }
+            }
+        }            
     }
+}
